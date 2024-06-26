4b06c1b2e3cd857db80e128ba5c453ddb7c54297
Ludodef <defazioludovica95@gmai.com>
2024-06-26 14:32:43 +0200

    rinizio progetto

---
+++ shop_libri/src/main/java/it/epicode/shop_libri/ShopLibriApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.shop_libri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopLibriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopLibriApplication.class, args);
+	}
+
+}
---
+++ shop_libri/src/test/java/it/epicode/shop_libri/ShopLibriApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.shop_libri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopLibriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
